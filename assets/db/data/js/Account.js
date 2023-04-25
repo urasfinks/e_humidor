73bf5415a6edfbe144c270e61a2f931c39fc05df
sfinks <urasfinks@yandex.ru>
2023-04-25 21:04:51 +0300

    Подготовка страницы Account

--- assets/db/data/js/Account.js
+++ assets/db/data/js/Account.js
@@ -1,3 +1,37 @@
 if (bridge.args["switch"] == "constructor") {
+    console.log("Hello constructor");
+}
 
+if (bridge.args["switch"] == "GetCodeResponse") {
+    console.log(bridge.args);
+    // if(bridge.args["body"]["status"] == false){
+    //     bridge.call("Alert", {
+    //         "duration": 7000,
+    //         "label": bridge.args["body"]["exception"].join(", ")
+    //     });
+    // }
+    bridge.call("NavigatorPush", {
+        "name": "Humidor",
+        "modalBottomSheet": true,
+        "link": {
+            "template": "AccountCode.json",
+        }
+    });
+
+}
+
+if (bridge.args["switch"] == "GetCode") {
+    //console.log(bridge.state);
+    bridge.call('Http', {
+        "uri": "/getcode",
+        "body": {
+            "mail": bridge.state["EmailValue"]
+        },
+        "onResponse": {
+            "jsInvoke": "Account.js",
+            "args": {
+                "switch": "GetCodeResponse"
+            }
+        }
+    });
 }
