8793b1cc8c5b13b43bc51108b7aae865e57e632a
sfinks <urasfinks@yandex.ru>
2023-04-27 23:03:44 +0300

    Подготовка к авторизации

--- assets/db/data/js/Account.js
+++ assets/db/data/js/Account.js
@@ -2,26 +2,7 @@ if (bridge.args["switch"] == "constructor") {
     console.log("Hello constructor");
 }
 
-if (bridge.args["switch"] == "GetCodeResponse") {
-    console.log(bridge.args);
-    // if(bridge.args["body"]["status"] == false){
-    //     bridge.call("Alert", {
-    //         "duration": 7000,
-    //         "label": bridge.args["body"]["exception"].join(", ")
-    //     });
-    // }
-    bridge.call("NavigatorPush", {
-        "name": "Humidor",
-        "modalBottomSheet": true,
-        "link": {
-            "template": "AccountCode.json",
-        }
-    });
-
-}
-
 if (bridge.args["switch"] == "GetCode") {
-    //console.log(bridge.state);
     bridge.call('Http', {
         "uri": "/getcode",
         "body": {
@@ -30,8 +11,57 @@ if (bridge.args["switch"] == "GetCode") {
         "onResponse": {
             "jsInvoke": "Account.js",
             "args": {
+                "includeStateData": true,
                 "switch": "GetCodeResponse"
             }
         }
     });
+}
+
+if (bridge.args["switch"] == "GetCodeResponse") {
+    console.log(bridge.args);
+    if (bridge.args["body"]["status"] == false) {
+        bridge.call("Alert", {
+            "duration": 7000,
+            "label": bridge.args["body"]["exception"].join(", ")
+        });
+    } else {
+        bridge.call("NavigatorPush", {
+            "name": "Humidor",
+            "modalBottomSheet": true,
+            "mail": bridge.state["EmailValue"],
+            "link": {
+                "template": "AccountCode.json",
+            }
+        });
+    }
+}
+
+if (bridge.args["switch"] == "ConfirmCode") {
+    console.log(bridge.state);
+    bridge.call('Http', {
+        "uri": "/signin",
+        "body": {
+            "code": bridge.state["CodeValue"] * 1,
+            "mail": bridge.pageArgs["mail"]
+        },
+        "onResponse": {
+            "jsInvoke": "Account.js",
+            "args": {
+                "switch": "ConfirmCodeResponse"
+            }
+        }
+    });
+}
+
+if (bridge.args["switch"] == "ConfirmCodeResponse") {
+    console.log(bridge.args);
+    if (bridge.args["body"]["status"] == false) {
+        bridge.call("Alert", {
+            "duration": 7000,
+            "label": bridge.args["body"]["exception"].join(", ")
+        });
+    } else {
+
+    }
 }
