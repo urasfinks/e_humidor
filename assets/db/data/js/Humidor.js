a5aaa96b1609e15aa19d67c7f9073f3b226bf9d7
sfinks <urasfinks@yandex.ru>
2023-05-21 23:02:42 +0300

    Переписано отображение сигар на Iterator.

--- assets/db/data/js/Humidor.js
+++ assets/db/data/js/Humidor.js
@@ -43,222 +43,29 @@ if (bridge.args["switch"] == "checkCountData") {
         }
         brand[curBrand].push(bridge.args["fetchDb"][i]);
     }
-    let tplCigar2 = {
-        "flutterType": "Container",
-        "padding": 18,
-        "child": {
-            "flutterType": "Row",
-            "newContext": false,
-            "crossAxisAlignment": "start",
-            "children": [
-                {
-                    "flutterType": "Expanded",
-                    "child": {
-                        "flutterType": "Text",
-                        "label": "Линейка: ${value_data.line}; Форма: ${value_data.shape}; Формат: ${value_data.format}\n${value_data.timestamp|timestampToDate(dd.MM.yyyy)}"
-                    }
-                },
-                {
-                    "flutterType": "SizedBox",
-                    "width": 20
-                },
-                {
-                    "flutterType": "Text",
-                    "label": "${value_data.size}"
-                }
-            ]
-        }
-    };
-    let tplCigar = {
-        "flutterType": "Margin",
-        "margin": "10,0,10,0",
-        "child": {
-            "flutterType": "Material",
-            "color": "schema:onBackground",
-            "child": {
-                "flutterType": "InkWell",
-                "onTap": {},
-                "customBorder": {
-                    "flutterType": "RoundedRectangleBorder",
-                    "borderRadius": 5
-                },
-                "child": {
-                    "flutterType": "Container",
-                    "margin": "15,2,15,2",
-                    "child": {
-                        "flutterType": "Slidable",
-                        "newContext": false,
-                        "children": [
-                            {
-                                "flutterType": "SizedBox",
-                                "width": 100,
-                                "child": {
-                                    "flutterType": "Row",
-                                    "mainAxisAlignment": "center",
-                                    "newContext": false,
-                                    "children": [
-                                        {
-                                            "flutterType": "RawMaterialButton",
-                                            "fillColor": "red",
-                                            "padding": 10,
-                                            "onPressed": {
-                                                "jsInvoke": "Humidor.js",
-                                                "args": {
-                                                    "switch": "onRemove",
-                                                    "uuid_cigar": "${uuid_data}",
-                                                    "templateArguments": [
-                                                        "uuid_cigar"
-                                                    ]
-                                                }
-                                            },
-                                            "icon": {
-                                                "flutterType": "Icon",
-                                                "src": "delete_outline",
-                                                "color": "white",
-                                                "size": 20
-                                            }
-                                        }
-                                    ]
-                                }
-                            }
-                        ],
-                        "child": {
-                            "flutterType": "Row",
-                            "newContext": false,
-                            "crossAxisAlignment": "start",
-                            "children": [
-                                {
-                                    "flutterType": "Expanded",
-                                    "child": {
-                                        "flutterType": "Column",
-                                        "crossAxisAlignment": "start",
-                                        "newContext": false,
-                                        "children": [
-                                            {
-                                                "flutterType": "Text",
-                                                "label": "${value_data.line}; Формат: ${value_data.shape} - ${value_data.format}",
-                                                "style": {
-                                                    "flutterType": "TextStyle",
-                                                    "fontSize": 15
-                                                }
-                                            },
-                                            {
-                                                "flutterType": "Opacity",
-                                                "opacity": 0.4,
-                                                "child": {
-                                                    "flutterType": "Text",
-                                                    "label": "${value_data.timestamp|timestampToDate(dd.MM.yyyy)}"
-                                                }
-                                            }
-                                        ]
-                                    }
-                                },
-                                {
-                                    "flutterType": "SizedBox",
-                                    "width": 20
-                                },
-                                {
-                                    "flutterType": "Text",
-                                    "label": "${value_data.size}"
-                                }
-                            ]
-                        }
-                    }
-                }
-            }
-        }
-    };
+
     for (let key in brand) {
         result.push({
-            "template": {
-                "flutterType": "Container",
-                "margin": "22,17,22,17",
-                "child": {
-                    "flutterType": "Row",
-                    "newContext": false,
-                    "crossAxisAlignment": "start",
-                    "children": [
-                        {
-                            "flutterType": "Expanded",
-                            "child": {
-                                "flutterType": "Text",
-                                "label": key,
-                                "style": {
-                                    "flutterType": "TextStyle",
-                                    "fontSize": 17,
-                                    "fontWeight": "bold"
-                                }
-                            }
-                        },
-                        {
-                            "flutterType": "SizedBox",
-                            "width": 20
-                        },
-                        {
-                            "flutterType": "Text",
-                            "label": brand[key].length
-                        }
-                    ]
-                }
-            }
-        });
-        result.push({
-            "template": {
-                "flutterType": "Container",
-                "margin": "10,0,10,0",
-                "height": 10,
-                "width": "infinity",
-                "decoration": {
-                    "flutterType": "BoxDecoration",
-                    "color": "schema:onBackground",
-                    "borderRadius": "10,10,0,0"
-                }
-            }
+            "templateWidgetSrc": "CigarBrandPreview",
+            "customSeqType": "template",
+            "label": key,
+            "count": brand[key].length
         });
         for (let i = 0; i < brand[key].length; i++) {
-            brand[key][i]["template"] = tplCigar;
+            brand[key][i]["templateWidgetSrc"] = "CigarPreview";
+            if (brand[key].length == 1) {
+                brand[key][i]["customSeqType"] = "template_single";
+            } else if (i == 0) {
+                brand[key][i]["customSeqType"] = "template_first";
+            } else if (i == brand[key].length - 1) {
+                brand[key][i]["customSeqType"] = "template_last";
+            } else {
+                brand[key][i]["customSeqType"] = "template_middle";
+            }
             result.push(brand[key][i]);
-            if (i < brand[key].length - 1) {
-                result.push({
-                    "template": {
-                        "flutterType": "Container",
-                        "margin": "10,0,10,0",
-                        "height": 10,
-                        "color": "schema:onBackground"
-                    }
-                });
-                result.push({
-                    "template": {
-                        "flutterType": "Divider",
-                        "thickness": 1,
-                        "height": 1,
-                        "color": "schema:background"
-                    }
-                });
-                result.push({
-                    "template": {
-                        "flutterType": "Container",
-                        "margin": "10,0,10,0",
-                        "height": 10,
-                        "color": "schema:onBackground"
-                    }
-                });
-            }
         }
-        result.push({
-            "template": {
-                "flutterType": "Container",
-                "margin": "10,0,10,0",
-                "height": 10,
-                "width": "infinity",
-                "decoration": {
-                    "flutterType": "BoxDecoration",
-                    "color": "schema:onBackground",
-                    "borderRadius": "0,0,10,10"
-                }
-            }
-        });
     }
+
     //console.log(brand);
     bridge.call('SetStateData', {
         "key": "CigarList",
